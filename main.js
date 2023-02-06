49d0bad424bd65d81c248a06d87018bc3e22de7c
Daiki Yamamoto <0ey329231k1095v@gmail.com>
2023-02-05 22:46:19 -0800

    modified

--- main.js
+++ main.js
@@ -11,3 +11,28 @@ const mySwiper = new Swiper('.swiper', {
     disableOnInteraction: false,
   },
 })
+
+const cartBtn = document.querySelector('#cart')
+const sidebarCart = document.querySelector('.cart-box')
+const body = document.querySelector('body')
+
+const closeBg = document.querySelector('.close-bg')
+const closeBtn = document.querySelector('#close-btn')
+
+cartBtn.addEventListener('click', function () {
+  body.classList.add('active')
+  closeBg.classList.add('active')
+  sidebarCart.classList.add('active')
+})
+
+closeBtn.addEventListener('click', function () {
+  body.classList.remove('active')
+  sidebarCart.classList.remove('active')
+  closeBg.classList.remove('active')
+})
+
+closeBg.addEventListener('click', function () {
+  body.classList.remove('active')
+  sidebarCart.classList.remove('active')
+  this.classList.remove('active')
+})
